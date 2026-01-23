a4121c4c3ea4606b47b4b79898071c9a0932734e
lying200 <lying200@outlook.com>
2026-01-23 09:46:05 +0800

    feat: 通过 nixos config 直接管理 IDEA

--- modules/system/apps.nix
+++ modules/system/apps.nix
@@ -2,11 +2,20 @@
 
 {
   environment.systemPackages = with pkgs; [
-    jetbrains-toolbox
+    # IntelliJ IDEA Ultimate with Wayland support (使用 unstable 保持最新)
+    (jetbrains.idea.override {
+      vmopts = ''
+        -Dawt.toolkit.name=WLToolkit
+      '';
+    })
     vscode
     wechat
     google-chrome
     gemini-cli
   ];
-  
+
+  # IntelliJ IDEA Wayland 支持配置
+  environment.sessionVariables = {
+    _JAVA_AWT_WM_NONREPARENTING = "1";  # 修复 tiling WM 问题
+  };
 }
