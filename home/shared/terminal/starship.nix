224c48509fa90d23c6982c2c64d194ea722ee40c
lying200 <lying200@outlook.com>
2026-03-30 23:50:14 +0800

    feat: 切换 starship 主题至 Catppuccin Powerline

--- home/shared/terminal/starship.nix
+++ home/shared/terminal/starship.nix
@@ -6,9 +6,9 @@
     enableFishIntegration = true;
   };
 
-  # 使用 Catppuccin Mocha 主题预设（与全局主题统一）
+  # 使用 Catppuccin Powerline 主题预设（与全局主题统一）
   home.activation.starshipPreset = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${pkgs.starship}/bin/starship preset catppuccin-mocha -o $HOME/.config/starship.toml
+    ${pkgs.starship}/bin/starship preset catppuccin-powerline -o $HOME/.config/starship.toml
 
     # 替换 Apple 图标为 NixOS 雪花图标
     ${pkgs.gnused}/bin/sed -i 's/󰀵/󱄅/g' $HOME/.config/starship.toml
