3461004df13d8b1a63bdb000cc754a4c39d52812
lying200 <lying200@outlook.com>
2026-06-19 10:16:45 +0800

    fix(gnome): 对齐 niri 快捷键

--- home/desktop/programs/keybindings.nix
+++ home/desktop/programs/keybindings.nix
@@ -4,14 +4,26 @@
   dconf.settings = {
     "org/gnome/desktop/wm/keybindings" = {
       activate-window-menu = ["<Ctrl><Alt><Shift>w"];
+      close = ["<Super>q" "<Alt>F4"];
+    };
+
+    "org/gnome/shell/keybindings" = {
+      toggle-application-view = ["<Super>d"];
+    };
+
+    "org/gnome/shell/extensions/clipboard-indicator" = {
+      toggle-menu = ["<Super>y"];
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
       terminal = [];
+      screensaver = ["<Super>l" "<Super><Alt>l"];
 
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
       ];
     };
 
@@ -24,7 +36,19 @@
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
       name = "Open Terminal (Zellij)";
       command = "ghostty -e zellij";
-      binding = "<Control><Alt>t";
+      binding = "<Super>t";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      name = "File Manager";
+      command = "nautilus";
+      binding = "<Super>e";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      name = "Browser";
+      command = "google-chrome-stable";
+      binding = "<Super>b";
     };
   };
 }
