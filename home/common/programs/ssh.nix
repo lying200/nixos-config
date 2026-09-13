baf9cd1da4e485edf863bd5cc8a6317f67a85789
lying200 <lying200@outlook.com>
2026-09-13 22:58:38 +0800

    feat(ssh): 添加 Oracle 和 Rainyun 主机别名

--- home/common/programs/ssh.nix
+++ home/common/programs/ssh.nix
@@ -4,6 +4,20 @@ _: {
     enableDefaultConfig = false;
 
     settings = {
+      "oracle" = {
+        HostName = "144.24.39.57";
+        User = "ubuntu";
+        IdentityFile = "~/.ssh/id_ed25519";
+        IdentitiesOnly = true;
+      };
+
+      "rainyun" = {
+        HostName = "38.22.90.105";
+        User = "root";
+        IdentityFile = "~/.ssh/id_ed25519";
+        IdentitiesOnly = true;
+      };
+
       "github.com" = {
         HostName = "ssh.github.com";
         Port = 443;
