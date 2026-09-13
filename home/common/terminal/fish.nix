876c8c5e240cd1e42feab1d4396188ae8701e672
lying200 <lying200@outlook.com>
2026-09-13 20:27:42 +0800

    fix(terminal): 提高目录与 Zellij 配色对比度

--- home/common/terminal/fish.nix
+++ home/common/terminal/fish.nix
@@ -12,7 +12,9 @@ in {
     interactiveShellInit = ''
       set -g fish_greeting
 
-      set -gx LS_COLORS 'di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+      # True-color entries stay readable on dark terminal backgrounds.
+      set -gx LS_COLORS 'di=1;38;2;137;180;250:ln=38;2;148;226;213:so=38;2;245;194;231:pi=38;2;249;226;175:ex=38;2;166;227;161:bd=38;2;249;226;175:cd=38;2;249;226;175:or=1;38;2;243;139;168:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+      set -gx EZA_COLORS "$LS_COLORS"
 
       fish_vi_key_bindings
 
